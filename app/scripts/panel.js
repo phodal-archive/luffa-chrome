1dd91467c8da4ae7e0baefbd669e109c13cf75b1
Fengda HUANG <h@phodal.com>
2015-12-12 15:25:31 +0800

    add panel

---
+++ app/scripts/panel.js
@@ -0,0 +1,19 @@
+'use strict';
+
+chrome.devtools.panels.create('Luffa', '', 'panel.html', function (panel) {
+  var reactPanel = null;
+  panel.onShown.addListener(function (window) {
+    // when the user switches to the panel, check for an elements tab
+    // selection
+    window.panel.getNewSelection();
+    reactPanel = window.panel;
+    reactPanel.resumeTransfer();
+  });
+  panel.onHidden.addListener(function () {
+    if (reactPanel) {
+      reactPanel.hideHighlight();
+      reactPanel.pauseTransfer();
+    }
+  });
+});
+//# sourceMappingURL=panel.js.map
